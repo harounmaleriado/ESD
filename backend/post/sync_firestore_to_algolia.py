9a164a8bb72afbf5dcb33c1eb87e1fbd44e53456
TeckYew <teckyew2000@hotmail.com>
2024-03-29 20:20:53 +0800

    added errors for incomplete fields and ensure correct adding posts protocol

--- backend/post/sync_firestore_to_algolia.py
+++ backend/post/sync_firestore_to_algolia.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate('./firebase-adminsdk.json')
+cred = credentials.Certificate('firebase-adminsdk.json')
 firebase_admin.initialize_app(cred)
 
 # Initialize Firestore
